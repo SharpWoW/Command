4c7dce0e13967324b7b2a994e29d6fa6a494c263
F16Gaming <adam.hellberg95@gmail.com>
2011-10-23 01:39:10 +0200

    Added license info.

--- PlayerManager.lua
+++ PlayerManager.lua
@@ -1,5 +1,20 @@
 --[[
-	{OUTPUT_LICENSE_SHORT}
+	* Copyright (c) 2011 by Adam Hellberg.
+	* 
+	* This file is part of Command.
+	* 
+	* Command is free software: you can redistribute it and/or modify
+	* it under the terms of the GNU General Public License as published by
+	* the Free Software Foundation, either version 3 of the License, or
+	* (at your option) any later version.
+	* 
+	* Command is distributed in the hope that it will be useful,
+	* but WITHOUT ANY WARRANTY; without even the implied warranty of
+	* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	* GNU General Public License for more details.
+	* 
+	* You should have received a copy of the GNU General Public License
+	* along with Command. If not, see <http://www.gnu.org/licenses/>.
 --]]
 
 local MODE_BLACKLIST = 0
